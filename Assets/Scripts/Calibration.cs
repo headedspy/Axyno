00b11ea92e0fc48d046689ea75d011358d071521
headedspy <jackroster222@gmail.com>
2019-02-26 21:28:57 +0200

    Calibration menu added

---
+++ Assets/Scripts/Calibration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calibration : MonoBehaviour {
+	public Texture BoxTexture;
+
+    void OnGUI(){
+		GUI.backgroundColor = new Color(0f, 0f, 0f, 0f);
+        GUI.Box(new Rect((Screen.width/2f) - 25f/2, (Screen.height/2f) - 25f/2, 25f, 25f), BoxTexture);
+    }
+	
+	public void Update(){
+		if(Input.GetMouseButton(0)){
+			Destroy(gameObject);
+		}
+	}
+}
